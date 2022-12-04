d41fe1e3650ac720ba94424acbe21d4ed654ab04
YutaShimoka <118116907+YutaShimoka@users.noreply.github.com>
2022-12-05 01:52:36 +0900

    テストケース追加

--- src/UtilsTest.java
+++ src/UtilsTest.java
@@ -9,6 +9,7 @@ public class UtilsTest {
         assertEquals("5e8ff9bf55ba3508199d22e984129be6", Utils.md5sum("sample"));
         assertNull(Utils.md5sum(null));
         assertEquals("d41d8cd98f00b204e9800998ecf8427e", Utils.md5sum(""));
+        assertEquals("7215ee9c7d9dc229d2921a40e899ec5f", Utils.md5sum(" "));
         assertEquals("1920baf9665480a79df488e00e69530a", Utils.md5sum("　"));
     }
 
